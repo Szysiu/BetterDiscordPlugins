0b7a150f13827dc29b6ecea411cd2240b2315a22
Szysiu <lukasz.szysiak75@gmail.com>
2025-03-31 22:53:04 +0200

    Added error handling

--- RestoreOldUserMenu/RestoreOlduserMenu.plugin.js
+++ RestoreOldUserMenu/RestoreOlduserMenu.plugin.js
@@ -1,6 +1,6 @@
 /**
  * @name RestoreOldUserMenu
- * @version 1.0
+ * @version 1.1
  * @description Restore the old user menu style
  * @updateUrl https://raw.githubusercontent.com/Szysiu/BetterDiscordPlugins/main/RestoreOldUserMenu/RestoreOlduserMenu.plugin.jsgi
  * @author Szysiu#1878
@@ -12,16 +12,20 @@ module.exports = class RestoreOldUserMenu {
     }
 
     start() {
-        this.adjustMenuWidth();
+        this.adjustMenuWidth()
     }
 
     adjustMenuWidth() {
-        const leftPannelWidth = document.querySelector('.itemsContainer_ef3116').offsetWidth;
-        const menu = document.querySelector('.panels_c48ade');
+        const leftPannelWidth = document.querySelector('.itemsContainer_ef3116').offsetWidth
+        const menu = document.querySelector('.panels_c48ade')
 
-        menu.style.setProperty("left", `${leftPannelWidth}px`, "important");
-        menu.style.setProperty("border-radius", '0px', "important");
-        menu.style.setProperty("box-sizing", 'content-box', "important");
+        if(!menu || leftPannelWidth) {
+            console.error('Required DOM elements not found')
+        }
+
+        menu.style.setProperty("left", `${leftPannelWidth}px`, "important")
+        menu.style.setProperty("border-radius", '0px', "important")
+        menu.style.setProperty("box-sizing", 'content-box', "important")
 
         this.sidebarListOnResize(menu)
     }
